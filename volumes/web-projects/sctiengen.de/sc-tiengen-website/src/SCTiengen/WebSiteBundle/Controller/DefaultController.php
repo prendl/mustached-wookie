5f8be5d206f28bb4eeb8bbc5a853ebbb78491fdc
Peter Rendl <pr@rendl.net>
2015-01-12 00:23:55 +0100

    fixed route of home page

--- volumes/web-projects/sctiengen.de/sc-tiengen-website/src/SCTiengen/WebSiteBundle/Controller/DefaultController.php
+++ volumes/web-projects/sctiengen.de/sc-tiengen-website/src/SCTiengen/WebSiteBundle/Controller/DefaultController.php
@@ -2,12 +2,25 @@
 
 namespace SCTiengen\WebSiteBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('SCTiengenWebSiteBundle:Default:index.html.twig', array('name' => $name));
+    
+    public function homeAction(Request $request) {
+    	$dm = $this->get('doctrine_phpcr')->getManager();
+    	$contentRoot = $dm->find('Doctrine\ODM\PHPCR\Document\Generic', '/cms/content');
+    	$contentDocument = $contentRoot->getChildren()->first();
+    	
+    	if (!$contentDocument) {
+    		throw $this->createNotFoundException('No homepage configured');
+    	}
+    	
+    	$contentController = $this->get('cmf_content.controller');
+        $contentTemplate = 'SCTiengenWebSiteBundle:Page:home.html.twig';
+
+        return $contentController->indexAction($request, $contentDocument, $contentTemplate);
     }
 }
