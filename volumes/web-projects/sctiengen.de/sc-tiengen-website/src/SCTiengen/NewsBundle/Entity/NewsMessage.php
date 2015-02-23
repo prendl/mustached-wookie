d599873e057a45533d469c622c2c3e8b8a2a99a8
Peter Rendl <pr@rendl.net>
2015-02-23 23:48:51 +0100

    Added Doctine ORM config Generated NewsMessage getter/setter methods

--- volumes/web-projects/sctiengen.de/sc-tiengen-website/src/SCTiengen/NewsBundle/Entity/NewsMessage.php
+++ volumes/web-projects/sctiengen.de/sc-tiengen-website/src/SCTiengen/NewsBundle/Entity/NewsMessage.php
@@ -21,7 +21,7 @@ class NewsMessage {
 	 */
 	protected $title;
 	/**
-	 * @ORM\Column(type="text", nullable)
+	 * @ORM\Column(type="text", nullable=true)
 	 */
 	protected $summary;
 	/**
@@ -33,14 +33,160 @@ class NewsMessage {
 	 */
 	protected $publicationDate;
 	/**
-	 * @ORM\Column(type="datetime", nullable)
+	 * @ORM\Column(type="datetime", nullable=true)
 	 */
 	protected $publishStartDate;
 	/**
-	 * @ORM\Column(type="datetime", nullable)
+	 * @ORM\Column(type="datetime", nullable=true)
 	 */
 	protected $publishEndDate;
 	
-}
 
-?>
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return NewsMessage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     * @return NewsMessage
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string 
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return NewsMessage
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set publicationDate
+     *
+     * @param \DateTime $publicationDate
+     * @return NewsMessage
+     */
+    public function setPublicationDate($publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate
+     *
+     * @return \DateTime 
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * Set publishStartDate
+     *
+     * @param \DateTime $publishStartDate
+     * @return NewsMessage
+     */
+    public function setPublishStartDate($publishStartDate)
+    {
+        $this->publishStartDate = $publishStartDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publishStartDate
+     *
+     * @return \DateTime 
+     */
+    public function getPublishStartDate()
+    {
+        return $this->publishStartDate;
+    }
+
+    /**
+     * Set publishEndDate
+     *
+     * @param \DateTime $publishEndDate
+     * @return NewsMessage
+     */
+    public function setPublishEndDate($publishEndDate)
+    {
+        $this->publishEndDate = $publishEndDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publishEndDate
+     *
+     * @return \DateTime 
+     */
+    public function getPublishEndDate()
+    {
+        return $this->publishEndDate;
+    }
+}
