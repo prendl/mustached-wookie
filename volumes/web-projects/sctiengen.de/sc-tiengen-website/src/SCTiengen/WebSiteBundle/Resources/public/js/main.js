9794777832119913ab247766843541e40b6535c6
Peter Rendl <pr@rendl.net>
2015-05-12 22:48:33 +0200

    open external links in new window

--- volumes/web-projects/sctiengen.de/sc-tiengen-website/src/SCTiengen/WebSiteBundle/Resources/public/js/main.js
+++ volumes/web-projects/sctiengen.de/sc-tiengen-website/src/SCTiengen/WebSiteBundle/Resources/public/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('.trigger-toggle-login-box').on('click', function($event) {
-	  $event.preventDefault();
-	  $('#login-box').toggle();
+    $event.preventDefault();
+    $('#login-box').toggle();
   });
+  
+  $('a[href^="http://"]').attr('target', '_blank');
 });
